ebc83d679e9d9d6ca61af30a4d0656662cc77f17
Keyur <keyur.raval18@gmail.com>
2020-03-27 09:39:37 +0530

    Test Capacity of Kubes

--- siteConfig.js
+++ siteConfig.js
@@ -1,5 +1,5 @@
 module.exports = {
-  siteUrl: "https://yourwordpressblog.com", // Site domain. Do not include a trailing slash!
+  siteUrl: "https://ipldreampredictions.com", // Site domain. Do not include a trailing slash!
 
   postsPerPage: 2, // Number of posts shown on paginated pages (changes this requires sometimes to delete the cache)
 
