f9b4cc8c554229ba6fb453e5968b9f01299e28cb
Keyur <keyur.raval18@gmail.com>
2020-05-16 13:41:02 +0530

    Adds protocol

--- gatsby-config.js
+++ gatsby-config.js
@@ -10,8 +10,10 @@ try {
 
 console.log(typeof process.env.WP_HOSTING_WPCOM);
 const hostingWPCOM = process.env.WP_HOSTING_WPCOM &&  process.env.WP_HOSTING_WPCOM === "true" ? true: false; 
+
 if (process.env.WP_BASE_URL) {
   wordpressConfig.baseUrl = process.env.WP_BASE_URL; 
+  wordpressConfig.protocol = process.env.WP_PROTOCOL ? process.env.WP_PROTOCOL : "https";
   if (hostingWPCOM) {
     wordpressConfig.hostingWPCOM = true;
     wordpressConfig.auth = {};
