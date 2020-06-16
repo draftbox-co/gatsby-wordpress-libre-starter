b86101c36de1e61f8b9fb7f56218406f0813a197
Keyur <keyur.raval18@gmail.com>
2020-06-16 19:00:05 +0530

    Update underlying package

--- siteConfig.js
+++ siteConfig.js
@@ -4,8 +4,7 @@ module.exports = {
   postsPerPage: 10, // Number of posts shown on paginated pages (changes this requires sometimes to delete the cache)
 
   siteTitleMeta: "Built with Draftbox", // This allows an alternative site title for meta data for pages.
-  siteDescriptionMeta:
-    "New-age blogging platform", // This allows an alternative site description for meta data for pages.
+  siteDescriptionMeta: "New-age blogging platform", // This allows an alternative site description for meta data for pages.
 
   shareImageWidth: 1000, // Change to the width of your default share image
   shareImageHeight: 523, // Change to the height of your default share image
@@ -13,5 +12,63 @@ module.exports = {
   shortTitle: "Built with Draftbox", // Used for App manifest e.g. Mobile Home Screen
   siteIcon: "favicon.png", // Logo in /static dir used for SEO, RSS, and App manifest
   backgroundColor: "#e9e9e9", // Used for Offline Manifest
-  themeColor: "#15171A" // Used for Offline Manifest
+  themeColor: "#15171A", // Used for Offline Manifest
+  apiUrl: "https://wordpress.theasdfghjkl.com",
+  subscribeWidget: {
+    title: "Subscribe to Draftbox",
+    helpText: "Get the latest posts delivered right to your inbox.",
+    successMessage: "Thanks for subscribing to Draftbox.",
+  },
+  header: {
+    navigation: [
+      {
+        label: "Home",
+        url: "https://wp-libre-preview.draftbox.co/",
+      },
+      {
+        label: "Contact",
+        url: "https://wp-libre-preview.draftbox.co/contact",
+      },
+      {
+        label: "Contact1111",
+        url: "https://wp-libre-preview.draftbox.co/contact111",
+      },
+    ],
+  },
+  footer: {
+    copyright: "Built with Draftbox",
+    navigation: [
+      {
+        label: "Home",
+        url: "https://wp-libre-preview.draftbox.co/",
+      },
+      {
+        label: "Sitemap",
+        url: "https://wp-libre-preview.draftbox.co/sitemap.xml",
+      },
+      {
+        label: "RSS",
+        url: "https://wp-libre-preview.draftbox.co/rss.xml",
+      },
+      {
+        label: "Contact",
+        url: "https://wp-libre-preview.draftbox.co/contact",
+      },
+      {
+        label: "External Link",
+        url: "https://spectrum.chat/gatsby-js/themes?tab=posts",
+      },
+    ],
+  },
+  socialLinks: {
+    twitter: "https://twitter.com/draftboxhq",
+    facebook: "https://facebook.com/",
+    instagram: "https://www.instagram.com/",
+    linkedin: "https://linkedin.com/",
+    github: "https://github.com/draftbox-co",
+  },
+  contactWidget: {
+    title: "Contact Built with Draftbox",
+    successMessage: "We’ll get in touch with you soon.",
+  },
 };
