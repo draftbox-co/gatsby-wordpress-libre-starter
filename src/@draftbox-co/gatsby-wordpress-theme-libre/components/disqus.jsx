0605d86bf2101f055906b1221b1582e0ac223834
Keyur <keyur.raval18@gmail.com>
2020-07-21 13:22:04 +0530

    Update for disqus, FB lazy loading

--- src/@draftbox-co/gatsby-wordpress-theme-libre/components/disqus.jsx
+++ src/@draftbox-co/gatsby-wordpress-theme-libre/components/disqus.jsx
@@ -1,15 +1,21 @@
 import React from "react";
 import { DiscussionEmbed } from "disqus-react";
 
-const Disqus = props => {
+const Disqus = (props) => {
   const disqusConfig = {
     shortname: process.env.GATSBY_DISQUS_SHORTNAME,
     identifier: props.slug,
-    title: props.slug
+    title: props.slug,
   };
 
   return process.env.GATSBY_DISQUS_SHORTNAME ? (
-    <DiscussionEmbed {...disqusConfig} />
+    <DiscussionEmbed
+      shortname={disqusConfig.shortname}
+      config={{
+        identifier: disqusConfig.identifier,
+        title: disqusConfig.title,
+      }}
+    />
   ) : (
     <></>
   );
